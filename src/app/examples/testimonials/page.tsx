0c17c01b9de80a17e4066e025abdeb45906ccca4
Rakibul Islam Sarkar <rakibulislam1019@gmail.com>
2025-01-18 00:18:33 +0530

    refactor(pages) : add to examples

---
+++ src/app/examples/testimonials/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next"
+import { TestimonialsClient } from "@/app/examples/testimonials/components/testimonials-client"
+
+export const metadata: Metadata = {
+  title: "Testimonials",
+  description: "Testimonials built using the components.",
+}
+
+export default function TestimonialsPage() {
+  return <TestimonialsClient />
+}
